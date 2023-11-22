501c7969c5caf055aded909a86ac6d19936dabfd
Xbostick <110611760+Xbostick@users.noreply.github.com>
2023-11-22 17:15:17 +0300

    Add files via upload

---
+++ hierarchy.cpp
@@ -0,0 +1,89 @@
+#include "hierarchy.h"
+#include <stdio.h>
+#include <string.h>
+/*-------Data---------*/
+Data::Data(char* raw_string, int len_string)
+{  
+    this->raw = new char[len_string];
+    memcpy (this->raw, raw_string, len_string);
+};
+
+ArrayType* Data::GetRawData()
+{
+    ArrayType* output = new ArrayType;
+    memcpy(output->data, this->raw, this->len_raw);
+    output->len=this->len_raw;
+    
+    return output;
+};
+
+ArrayType* Data::GetProcData()
+{
+    if (this->processed == NULL) return NULL;
+    
+    ArrayType* output = new ArrayType;
+    memcpy(output->data, this->processed, this->len_processed);
+    output->len=this->len_raw;
+    
+    return output;
+};
+
+char Data::NextProc()
+{   
+    if (this->processed == NULL) return NULL;
+    return *(this->processed + (this->processed_actual_index++));    
+};
+
+char Data::NextRaw()
+{   
+    return *(this->raw + (this->raw_actual_index++));    
+};
+
+char Data::GetProcIdx(int idx)
+{   
+    return *(this->processed + idx);    
+};
+
+char Data::GetRawIdx(int idx)
+{   
+    return *(this->raw + idx);    
+};
+
+/*-------Cluster---------*/
+ACluster::ACluster(AGovernor *governors_list, int governors_count, AProxy* proxy_list, int proxy_count)
+{
+    for (int i = 0; i < governors_count; i++)
+        this->AddGovernor(governors_list+i);
+    for (int i = 0; i < proxy_count; i++)
+        this->AddProxy(proxy_list+i);    
+};
+
+Data* ACluster:: Deligate2Governor(Data* data)
+{
+    TaskState status = TaskState::NotDeligated;
+    Governors_List* currentGoverner = this->Governors;
+
+    while (currentGoverner != NULL) {
+        status = currentGoverner->obj->CheckTask(data);
+        if (status != TaskState::NotDeligated)
+            break;
+		currentGoverner = currentGoverner->next;
+    }
+    if (status == TaskState::ExtraReturn)
+        {
+            Data* output = currentGoverner->obj->ProcessData(data);
+            return output;
+        }
+        
+    else
+        return NULL;
+};
+
+OperationStatus ACluster:: AddGovernor(AGovernor *new_module)
+{
+    Governors_List* newModule = new Governors_List;
+    newModule->obj = new_module;
+    newModule->next = this->Governors;
+    this->Governors = newModule;
+    return OperationStatus::OK;
+};
---
+++ hierarchy.h
@@ -0,0 +1,132 @@
+#ifndef HIERARCHY_H
+#define HIERARCHY_H
+
+class ACluster;     // Class to unite Governors
+class AGovernor;    // Class to determine tasks to it`s own commander and information exchange inside system
+class AProxy;       // Class without hardware/ Only operations inside governor
+class ACommander;   // Class to connect hardware. Knows its governor and hardware interface.
+
+
+class Data;        // Class to normalize data from every source
+struct ProcessedCmd;
+class IConverter;   // 
+
+struct ArrayType
+{
+    char* data;
+    int len;
+};
+
+enum TaskState
+{
+	NotDeligated = 0,
+	Deligated,
+	ExtraReturn
+};
+
+enum OperationStatus
+{
+	OK = 0,
+	Not_OK,
+	ErrorCode
+};
+
+
+class Data
+{   
+    private:
+    int raw_actual_index = 0;
+    int processed_actual_index = 0;
+    //int governor_code;
+    char *raw;
+    char *processed;
+    int len_processed;
+    int len_raw;
+
+
+    virtual OperationStatus SetGovernorCode()= 0;
+
+    public:
+    Data(char* raw_string, int len_string);
+
+    virtual OperationStatus CheckSum() = 0;
+    virtual ArrayType* GetRawData();
+    virtual ArrayType* GetProcData();
+
+    char NextProc();
+    char NextRaw();
+    
+    virtual ArrayType* NextArray() = 0;
+
+    char GetProcIdx(int idx);
+    char GetRawIdx(int idx);
+
+    
+};
+
+struct Governors_List {
+    AGovernor* 			    obj;
+    Governors_List* 		next;
+};
+
+struct Proxy_List {
+    AProxy* 			governor;
+    char*               ProxyName;
+    int                 NameLen;
+    Proxy_List* 		next;
+    
+    
+};
+// Возможно стоит использовать энам
+
+class ACluster
+{   
+    private:
+        Data*               Deligate2Governor(Data* data);
+        Governors_List*         Governors;
+        Proxy_List              Proxes;
+        int                     num_governors;
+        int                     num_proxy;
+
+    public:
+        ACluster(AGovernor *governors_list = 0, int governors_count = 0, AProxy* proxy_list = 0, int proxy_count = 0);
+        virtual Data*       ProcessData(Data* data);
+        OperationStatus     AddGovernor(AGovernor *new_module);
+        OperationStatus     AddProxy(AProxy *new_module, char* ProxyName, int NameLen);
+
+};
+
+class AGovernor
+{
+    private:
+        ACommander* Comander;
+        Proxy_List* Proxy;
+        Data* DataHolder;
+
+    public:
+        TaskState CheckTask(Data* data);
+        virtual Data* ProcessData(Data* data);
+        OperationStatus     AddComander(ACommander *new_module);
+        OperationStatus     AddProxy(AProxy *new_module, char* ProxyName, int NameLen);
+};
+
+class AProxy
+{
+    private:
+        Data* innerDataHandler;
+    public:
+        virtual Data* ProcessData(Data* data);
+};
+
+class ACommander
+{
+    private:
+
+    public:
+        virtual Data* ProcessData(Data* data);
+        
+};
+
+
+
+#endif
