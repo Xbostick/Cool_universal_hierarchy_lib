35105543b63d91ce6ae8ab332ade7763a15b7ea7
xbostick <borovkov-pavel@yandex.ru>
2023-12-05 17:24:10 +0300

    modified:   .gitignore 	modified:   hierarchy_general.cpp 	modified:   hierarchy_general.h

--- hierarchy_general.cpp
+++ hierarchy_general.cpp
@@ -1,11 +1,25 @@
 #include "hierarchy_general.h"
 
-FlashData::FlashData(char* raw_string,int len) : Data(raw_string, len)
-{
-};
 
-FlashData::FlashData(Data* data)
+FlashData::FlashData( char* raw_string = nullptr,int len = 0) : Data(raw_string, len)
 {
 
-
 };
+
+FlashData::FlashData(FlashMeta meta, char* raw_string = nullptr, int len = 0): Data(raw_string, len)
+{
+    this->_meta = meta;
+};
+/*--------------Governonr---------*/
+
+/*Creates new mapping data space after last mapped adress*/
+OperationStatus FlashGovernor::CreateMapped(FlashData* data)
+{
+    if (data->_meta.start == 0)
+        data->_meta.start = this->Mapped->data->start + 1;
+    FlashMap_List* block = new FlashMap_List;
+    block->data = &data->_meta;
+    block->next = this->Mapped;
+};
+
+OperationStatus FlashGovernor::DeleteMapped()
--- hierarchy_general.h
+++ hierarchy_general.h
@@ -3,6 +3,8 @@
 
 #include "hierarchy.h"
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 
 /*--------Data_General-------*/
 
@@ -10,10 +12,11 @@
 /*Обертка для записи-чтения из flash - памяти*/
 class FlashData : public Data
 {
+    private:
     public:
-        FlashMeta   data;
-        FlashData(char* raw_string, int len);
-        FlashData(Data* data);
+        FlashMeta   _meta;
+        FlashData(char* raw_string = nullptr, int len = 0);
+        FlashData(FlashMeta meta, char* raw_string = nullptr, int len = 0);
 };
 
 /*--------Clusters_General-------*/
@@ -28,6 +31,7 @@ class FlashGovernor : public AGovernor
         OperationStatus CreateMapped(FlashData* data);
         OperationStatus DeleteMapped(char* Name, int NameLen);
         OperationStatus DeleteMapped(int idx);
+        OperationStatus ReMappedMemmory();
         FlashData* TakeMapped(char* Name, int NameLen);
         FlashData* TakeMapped(int idx);
     
@@ -56,7 +60,7 @@ class FlashCommander: public ACommander
 /*--------Other_General-------*/
 
 struct FlashMeta{
-    int32_t 			start;
+    int32_t 			start; // sector number
     int32_t 			len;
     char*               Name;
     int                 NameLen;
@@ -67,6 +71,6 @@ struct FlashMeta{
 
 struct FlashMap_List{
 
-    FlashMeta           data;
+    FlashMeta*          data;
     FlashMap_List* 		next;    
 };
